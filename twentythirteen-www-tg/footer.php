0d17d90d3acd92590b3161b2e07fa87b5c9aecf6
Christian Vinh <christian.vinh@gmail.com>
2014-02-27 18:03:55 +0100

    initial commit: Homepage: all sections roughly OK but top one

---
+++ twentythirteen-www-tg/footer.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+?>
+
+</div><!-- #main -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="widget-area">
+        <aside id="footer-tg-logo" class="widget">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/tg-logo.png" alt=""/>
+            <h2>Tanaguru</h2>
+        </aside>
+        <aside id="footer-tg" class="widget">
+            <h2>Tanaguru</h2>
+            <ul>
+                <li>
+                    <a href="" title="<?php _e('Technology link title', 'wwwtg'); ?>"><?php _e('Technology link text', 'wwwtg'); ?></a>
+                </li>
+                <li>
+                    <a href="" title="<?php _e('Tour link title', 'wwwtg'); ?>"><?php _e('Tour link text', 'wwwtg'); ?></a>
+                </li>
+                <li>
+                    <a href="" title="<?php _e('Offer link title', 'wwwtg'); ?>"><?php _e('Offer link text', 'wwwtg'); ?></a>
+                </li>
+                <li>
+                    <a href="" title="<?php _e('Contact link title', 'wwwtg'); ?>">Contact</a>
+                </li>
+                <li>
+                   <a href="" title="<?php _e('Blog link title', 'wwwtg'); ?>">Blog</a>
+                </li>
+            </ul>
+        </aside>
+        <aside id="footer-and-also" class="widget">
+            <h2><?php _e('And also', 'wwwtg'); ?></h2>
+            <ul>
+                <li>
+                    <a href="<?php _e('KBAccess URL', 'wwwtg'); ?>" title="<?php _e('KBAccess link title', 'wwwtg'); ?>">KBAccess</a>
+                </li>
+                <li>
+                    <a href="<?php _e('Contrast-Finder URL', 'wwwtg'); ?>" title="<?php _e('Contrast-Finder link title', 'wwwtg'); ?>">Contrast-Finder</a>
+                </li>
+                <li>
+                    <a href="<?php _e('Observatory URL', 'wwwtg'); ?>"><?php _e('Observatory link text', 'wwwtg'); ?></a>
+                </li>
+                <li>
+                    <a href="<?php _e('Open-S URL', 'wwwtg'); ?>">Open-S</a>
+                </li>
+                <li>
+                    <a href="<?php _e('Press link', 'wwwtg'); ?>"><?php _e('Press link text', 'wwwtg'); ?></a>
+                </li>
+            </ul>
+        </aside>
+        <aside  id="footer-social" class="widget">
+            <h2 class="screen-reader-text"><?php _e('Social Tanaguru', 'wwwtg'); ?></h2>
+            <p>
+                <a href="<?php _e('Twitter link', 'wwwtg'); ?>" title="<?php _e('Twitter link title', 'wwwtg'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/Social/twitter.png" alt="<?php _e('Tanaguru on Twitter', 'wwwtg'); ?>" /></a>
+                <a href="<?php _e('Github link', 'wwwtg'); ?>" title="<?php _e('Github link title', 'wwwtg'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/Social/github.png" alt="<?php _e('Tanaguru on Github', 'wwwtg'); ?>" /></a> 
+                <a href="<?php _e('Linkedin link', 'wwwtg'); ?>" title="<?php _e('Linkedin link title', 'wwwtg'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/Social/linkedin.png" alt="<?php _e('Tanaguru on Linkedin', 'wwwtg'); ?>" /></a>
+                <a href="<?php _e('Slideshare link', 'wwwtg'); ?>" title="<?php _e('Slideshare link title', 'wwwtg'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/Social/slideshare.png" alt="<?php _e('Tanaguru on Slideshare', 'wwwtg'); ?>" /></a>
+                <a href="<?php _e('Ohloh link', 'wwwtg'); ?>" title="<?php _e('Ohloh link title', 'wwwtg'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/Social/ohloh.png" alt="<?php _e('Tanaguru on Ohloh', 'wwwtg'); ?>" /></a>
+            </p>
+               
+                
+        </aside>
+    </div><!-- /widget-area -->
+
+    <p id="footer2-1"><?php _e('Tanaguru free software by Open-S', 'wwwtg'); ?></p>
+    <p id="footer2-2">
+        <a href="<?php _e('Legal link', 'wwwtg'); ?>"><?php _e('Legal text link', 'wwwtg'); ?></a> | 
+        <a href="<?php _e('Credits link', 'wwwtg'); ?>"><?php _e('Credits text link', 'wwwtg'); ?></a> |
+        <a href="<?php _e('Accessibility link', 'wwwtg'); ?>"><?php _e('Accessibility text link', 'wwwtg'); ?></a>
+    </p>
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+</body>
+</html>
---
+++ twentythirteen-www-tg/front-page.php
@@ -0,0 +1,148 @@
+<?php
+
+get_header(); ?>
+
+<section id="everest">
+    <div id="everest_gradiant">
+        <div id="filter">
+        </div>
+    </div>    
+    <div id="everest_dragon">
+        <article id="accessibility_simply">
+            <div id="text_simply">
+                <h2>L'accessibilité. Simplement.</h2>
+                <p>La voie la plus rapide pour optimiser votre accessibilité.</p>
+            </div>    
+                <p class="center">
+                    <a href="#" id="button_simply" class="button"><?php _e('> Testez Tanaguru', 'wwwtg'); ?></a>
+                </p> 
+             
+        </article>
+
+        <article id="accessibility_fast">
+                <h2 class="center">Accélérez vos tests d’accessibilité. Vraiment.</h2>
+                <p class="center responsive"> Tanaguru , c’est l’analyse d’une page, d’un site entier ou même d’un parcours utilisateurs,
+                    simplement, rapidement, de manière fiable et intuitive. Puissant. Vraiment.</p>
+                <img class="tg-img-deco responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/Images/machine.png" alt=""/>
+        </article>     
+     </div>
+  
+
+</section>
+
+
+
+ 
+<section id="technology">
+    
+    <h2 class="center">Plusieurs types d’analyses. Des possibilités multiples.</h2>
+    <ul class="center">
+        <li id="techno1">
+            <h3 class="center">Site&Page</h3>
+            <p>By and by, through the glass the
+                stranger's boats and manned
+                mast-heads proved her a big awesome
+                whale-ship.</p>
+        </li> 
+        <li id="techno2">
+            <h3 class="center">Fichiers hors-ligne</h3>
+            <p>By and by, through the glass the
+                stranger's boats and manned
+                mast-heads proved her a big awesome
+                whale-ship.</p>
+        </li>  
+        <li id="techno3">
+            <h3 class="center">Scénario</h3>
+            <p>By and by, through the glass the
+                stranger's boats and manned
+                mast-heads proved her a big awesome
+                whale-ship.</p>
+        </li>  
+    </ul>
+    
+     <p class="center">
+        <a href="" id="button_technology" class="button"><?php _e('More about our technology', 'wwwtg'); ?></a>
+    </p> 
+    
+</section>
+
+<section id="why" class="">
+   
+        <h2 class="center">Pourquoi Tanaguru ?</h2> 
+    <ul class="center">
+        <li id="why1">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/data.png" alt=""/>
+            <h3>Fiable</h3>
+            <p>By and by, through the glass the
+                stranger's boats and manned mastheads
+                proved her a whale-ship.</p>
+        </li><!--
+        --><li id="why2">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/gears.png" alt=""/>
+            <h3>Automatisable</h3>
+            <p>By and by, through the glass the
+                stranger's boats and manned mastheads
+                proved her a whale-ship.</p>
+        </li><!--
+        --><li id="why3">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/flipflops.png" alt=""/>
+            <h3>Facile</h3>
+            <p>By and by, through the glass the
+                stranger's boats and manned mastheads
+                proved her a whale-ship.</p>
+        </li>
+    </ul>
+   
+</section>
+
+<section id="steps">
+    <h2 class="center">Easy. Fast. Adaptive. </h2>
+    <p class="center"> Avec Tanaguru, 4 étapes suffisent pour tester un site entier, de manière personnalisable, fiable et agréable.</p>
+    <img class="tg-img-deco responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/Images/steps.png"" alt=""/>
+    <ol class="center">
+        <li id="step1">Lancez un audit en le
+            parametrant suivant
+            vos besoins.</li>
+        <li id="step2">Définissez vos priorités
+            grâce à la synthèse globale.</li>
+        <li id="step3">A vous de jouer !
+            Utilisez les résultats
+            détaillés.</li>
+        <li id="step4">Suivez l’évolution
+            de l’accessibilité de
+            votre site.</li>
+    </ol>
+</section>
+
+<section id="references">
+    <h2 class="center">They<img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/heart.png" alt="love"/>Tanaguru</h2>
+    <p class="center"><?php _e('Slogan', 'wwwtg'); ?></p>
+    <p class="center">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/edf.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/smile.png" alt="" />
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/alterway.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/sg.png" alt=""/>
+    </p>   
+    <p class="center">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/capgemini.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/universcience.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/mairieparis.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/michelin.png" alt=""/> 
+    </p>
+    <p class="center">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/ministereculture.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/inpes.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/rff.png" alt=""/>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Images/References/agenceeau.png" alt=""/>     
+    </p>
+    
+</section>
+
+<section id="evaluate">
+    <p class="center">
+        <a href="" id="button_evaluate" class="button"><?php _e('Evaluate your accessibility !', 'wwwtg'); ?></a>
+    </p>
+  </section>
+
+
+<?php get_footer(); ?>
