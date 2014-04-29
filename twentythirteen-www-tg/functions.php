c4af550957254f5bf93eaf96a67b5ae5a1a029bd
Matthieu Faure <mfaure@tanaguru.com>
2014-04-29 13:49:08 +0200

    remove google font calls from parent theme

--- twentythirteen-www-tg/functions.php
+++ twentythirteen-www-tg/functions.php
@@ -1,8 +1,21 @@
 <?php
+
 /*
  * i18n
  */
 add_action('after_setup_theme', 'my_theme_setup');
-function my_theme_setup(){
+
+function my_theme_setup() {
     load_theme_textdomain('wwwtg', get_stylesheet_directory() . '/languages');
+}
+
+/*
+ * Remove or add CSS files
+ */
+add_action('wp_print_styles', 'child_overwrite_styles', 100);
+
+function child_overwrite_styles() {
+    // Remove the CSS file GoogleFonts 
+    wp_dequeue_style('twentythirteen-fonts');           // Remove a CSS file that was enqueued with wp_enqueue_style().
+    wp_deregister_style('twentythirteen-fonts');     // Remove a CSS file that was registered with wp_register_style().  
 }
