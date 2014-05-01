085518789670ad0e1a79c56da7fdfb9f2e632a0e
Matthieu Faure <mfaure@tanaguru.com>
2014-05-01 19:58:25 +0200

    prepared better sprite management

---
+++ twentythirteen-www-tg/internal-tools/sprites.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Achtung: glue is required to build sprites
+# https://glue.readthedocs.org/en/latest/installation.html#debian-ubuntu
+
+glue --source ../Images/Sprite-home/ --less ../Less/ --img ../Images/
