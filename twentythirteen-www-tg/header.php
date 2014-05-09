965a04730506fcd2d1c33e77e6c784aee0160826
Matthieu Faure <mfaure@tanaguru.com>
2014-05-09 22:28:08 +0200

    fixed skip link

--- twentythirteen-www-tg/header.php
+++ twentythirteen-www-tg/header.php
@@ -71,7 +71,7 @@
 
                     <nav id="site-navigation" class="navigation main-navigation" role="navigation">
                         <p class="menu-toggle"><?php _e('Menu', 'twentythirteen'); ?></p>
-                        <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e('Skip to content', 'twentythirteen'); ?>"><?php _e('Skip to content', 'twentythirteen'); ?></a>
+                        <a class="screen-reader-text skip-link" href="#content"><?php _e('Skip to content', 'twentythirteen'); ?></a>
                         <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav-menu')); ?>
 
                     </nav><!-- #site-navigation -->
